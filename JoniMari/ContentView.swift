9b8a799ea09f68ac679bfb9a0e8a3ccb79c4b1ea
unoyu <unosaan@gmail.com>
2024-05-28 08:28:52 +0900

    Initial Commit

---
+++ JoniMari/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JoniMari
+//
+//  Created by 宇野佑 on 2024/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JoniMari/JoniMariApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoniMariApp.swift
+//  JoniMari
+//
+//  Created by 宇野佑 on 2024/05/28.
+//
+
+import SwiftUI
+
+@main
+struct JoniMariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
